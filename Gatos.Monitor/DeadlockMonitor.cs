577f6947ab0f055b95bc4cc203efa1d5d72926fc
Sasha Goldshtein <goldshtn@gmail.com>
2017-07-09 15:51:56 +0000

    Deadlock detector + deadlock scenario

--- Gatos.Monitor/DeadlockMonitor.cs
+++ Gatos.Monitor/DeadlockMonitor.cs
@@ -1,20 +1,83 @@
-﻿using System;
+﻿using Microsoft.Diagnostics.Runtime;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Gatos.Monitor
 {
     public class DeadlockMonitor
     {
-        // TODO Periodically check if there's a deadlock by walking thread wait chains
-        //      and looking for cycles. Show the simple version using only CLRMD BlockingObject
-        //      here, and explain that msos has support for additional synchronization types.
+        private static readonly TimeSpan SampleInterval = TimeSpan.FromSeconds(10);
+
+        private Timer _sampleTimer;
 
         public static DeadlockMonitor Start()
         {
             return new DeadlockMonitor();
+        }
+
+        private DeadlockMonitor()
+        {
+            _sampleTimer = new Timer(FindDeadlocks, null, SampleInterval, SampleInterval);
+        }
+
+        private void WriteTraceLine(string line)
+        {
+            Trace.WriteLine($"[DeadlockMonitor] {line}");
+        }
+
+        private void FindDeadlocks(object dummy)
+        {
+            WriteTraceLine("Attempting to find deadlocks");
+            using (var target = DataTarget.AttachToProcess(Process.GetCurrentProcess().Id, 1000, AttachFlag.Passive))
+            {
+                var runtime = target.ClrVersions[0].CreateRuntime();
+                foreach (var thread in runtime.Threads)
+                {
+                    FindDeadlockStartingFrom(thread, new HashSet<uint>(), new Stack<string>());
+                }
+            }
+        }
+
+        private void FindDeadlockStartingFrom(ClrThread thread, HashSet<uint> visitedThreadIds, Stack<string> chain)
+        {
+            if (thread == null)
+                return;
+
+            if (visitedThreadIds.Contains(thread.OSThreadId))
+            {
+                WriteTraceLine("Deadlock found between the following threads:");
+                foreach (var entry in chain.Reverse())
+                {
+                    WriteTraceLine($"  {entry} -->");
+                }
+                WriteTraceLine($"  Thread {thread.OSThreadId}, DEADLOCK!");
+                return;
+            }
+
+            visitedThreadIds.Add(thread.OSThreadId);
+            string topStackTrace = String.Join("\n      ",
+                thread.StackTrace.Where(f => f.Kind == ClrStackFrameType.ManagedMethod)
+                                 .Select(f => f.DisplayString)
+                                 .Take(5));
+            chain.Push($"Thread {thread.OSThreadId} at {topStackTrace}");
+
+            foreach (var blockingObject in thread.BlockingObjects)
+            {
+                chain.Push($"{blockingObject.Reason} at {blockingObject.Object:X}");
+                foreach (var owner in blockingObject.Owners)
+                {
+                    FindDeadlockStartingFrom(owner, visitedThreadIds, chain);
+                }
+                chain.Pop();
+            }
+
+            chain.Pop();
+            visitedThreadIds.Remove(thread.OSThreadId);
         }
     }
 }
--- Gatos.Web/Controllers/HomeController.cs
+++ Gatos.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml;
@@ -24,16 +26,41 @@ namespace Gatos.Web.Controllers
             }
             doc.OuterXml.ToString();
 
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About us.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            GatherContactInformation();
+            ViewBag.Message = "Contact us.";
 
             return View();
         }
+
+        private void GatherContactInformation()
+        {
+            var hqDetails = new ContactDetails();
+            var officeDetails = new ContactDetails();
+            ProcessDetails(hqDetails, officeDetails);
+        }
+
+        private string ProcessDetails(ContactDetails first, ContactDetails second)
+        {
+            lock (first)
+            {
+                Task.Run(() => ProcessDetails(second, first));
+                Thread.Sleep(200);
+                lock (second)
+                {
+                    return "Merged contacts: " + first.ToString() + second.ToString();
+                }
+            }
+        }
+    }
+
+    internal class ContactDetails
+    {
     }
 }
